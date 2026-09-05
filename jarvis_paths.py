567af1e51cad69e602dea6566493fccccfd3c840
akshay b <72197492+lion-leo-code@users.noreply.github.com>
2026-09-05 19:47:56 +0530

    Update jarvis_paths.py

--- jarvis_paths.py
+++ jarvis_paths.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
 
-JARVIS_DIR = Path(r"C:\Users\aniak\Documents\J.A.R.V.I.S")
+JARVIS_DIR = Path(r"C:\Users\Public\Documents\J.A.R.V.I.S")
 JARVIS_DIR.mkdir(parents=True, exist_ok=True)
